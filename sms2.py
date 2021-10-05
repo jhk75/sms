295334628f00346c5df90b8d6506dd34b4a7e78d
Julius Kaplan <755675@gmail.com>
2021-10-05 08:39:38 +0300

    My initial ver.1.0

--- sms2.py
+++ sms2.py
@@ -1,20 +1,17 @@
+#Sending sms via CLI. Example: python3 sms.py 0547000000 test
+
 import time
 import os
 import sys
-#import telnetlib
-#import glob
 import serial
 import argparse
 
-#recipient = "0547362625"
-#message = "test msg"
-
+phone=serial.Serial('/dev/ttyUSB0', 115200, timeout=2)
 recipient = sys.argv[1]
 message = str(sys.argv[2])
 
 def info(phone):
     try:
-        phone=serial.Serial('/dev/ttyUSB0', 115200, timeout=2)
         time.sleep(0.5)
         phone.write(b'ATZ\r')
         time.sleep(0.5)
@@ -31,8 +28,7 @@ def info(phone):
 
 
 def main():
-    phone= serial.Serial('/dev/ttyUSB0', 115200, timeout=2)
-    print(info(phone))
+    info(phone)
 
 
 if __name__ == "__main__":
